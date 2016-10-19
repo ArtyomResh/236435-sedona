9f081c48001fcf07aa8a2511c439e40e8a878c93
Artyom Resh <artyomresh@gmail.com>
2016-10-19 13:06:09 +0400

    Настроил автоматизацию проекта

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,143 +1,151 @@
 "use strict";
 
 module.exports = function(grunt) {
-  require("load-grunt-tasks")(grunt);
+    require("load-grunt-tasks")(grunt);
 
-  grunt.initConfig({
-    less: {
-      style: {
-        files: {
-          "build/css/style.css": "less/style.less"
-        }
-      }
-    },
-    postcss: {
-      style: {
-        options: {
-          processors: [
-            require("autoprefixer")({browsers: [
-              "last 1 version",
-              "last 2 Chrome versions",
-              "last 2 Firefox versions",
-              "last 2 Opera versions",
-              "last 2 Edge versions"
-            ]})
-          ]
-        },
-        src: "build/css/*.css"
-      }
-    },
-
-    browserSync: {
-      server: {
-        bsFiles: {
-          src: [
-            "build/*.html",
-            "build/css/*.css"
-          ]
-        },
-        options: {
-          server: "build",
-          watchTask: true,
-          notify: false,
-          open: true,
-          ui: false
-        }
-      }
-    },
-
-    watch: {
-      style: {
-        files: ["less/**/*.less"],
-        tasks: ["less", "postcss", "cssmin"],
-        options: {
-          spawn: false
-        }
-      },
-      html: {
-        files: ["*.html"],
-        tasks: ["copy:html"]
-      }
-    },
-
-    svgstore: {
-      options: {
-        svg: {
-          style: "display: none"
-        }
-      },
-      symbols: {
+    grunt.initConfig({
+      less: {
+        style: {
           files: {
-            "build/img/icons-inline.svg": ["img/icons-inline/*.svg"]
+            "build/css/style.css": "less/style.less"
+          }
         }
-      }
-    },
-
-    svgmin: {
-      symbols: {
-        files: [{
-          expand: true,
-          src: ["build/img/icons-inline/*.svg"]
-        }]
-      }
-    },
-
-    copy: {
-      build: {
-        files: [{
-          expand: true,
-          src: [
-            "fonts/**/*.{woff,woff2}",
-            "img/**",
-            "js/**",
-            "*.html"
-          ],
-          dest: "build"
-        }]
       },
-      html: {
-        files: [{
-          expand: true,
-          src: ["*.html"],
-          dest: "build"
-        }]
-      }
-    },
 
-    clean: {
-      build: ["build"]
-    },
+      postcss: {
+        style: {
+          options: {
+            processors: [
+              require("autoprefixer")({browsers: [
+                "last 1 version",
+                "last 2 Chrome versions",
+                "last 2 Firefox versions",
+                "last 2 Opera versions",
+                "last 2 Edge versions"
+              ]})
+            ]
+          },
+          src: "build/css/*.css"
+        }
+      },
 
-    cssmin: {
-      style: {
-        files: {
-          "build/css/style.min.css": ["build/css/style.css"]
+      csso: {
+        style: {
+          options: {
+            report: "gzip"
+          },
+          files: {
+            "build/css/style.min.css": ["css/style.css"]
+          }
+        }
+      },
+
+      imagemin: {
+        images: {
+          options: {
+            optimizationLevel: 3
+          },
+          files: [{
+            expand: true,
+            src: ["build/img/**/*.{png,jpg,gif}"]
+          }]
+        }
+      },
+
+      clean: {
+        build: ["build/**"]
+      },
+
+      copy: {
+        build: {
+          files: [{
+            expand: true,
+            src: [
+              "fonts/**/*.{woff,woff2}",
+              "img/**",
+              "js/**",
+              "*.html"
+            ],
+            dest: "build"
+          }]
+        },
+        html: {
+          files: [{
+            expand: true,
+            src: ["*.html"],
+            dest: "build"
+          }]
+        }
+      },
+
+      browserSync: {
+        server: {
+          bsFiles: {
+            src: [
+              "build/*.html",
+              "build/css/*.css"
+            ]
+          },
+          options: {
+            server: "build",
+            watchTask: true,
+            notify: false,
+            open: true,
+            ui: false
+          }
+        }
+      },
+
+      watch: {
+        style: {
+          files: ["build/less/**/*.less"],
+          tasks: ["less", "postcss", ""],
+          options: {
+            spawn: false
+          }
+        },
+        html: {
+          files: ["*.html"],
+          tasks: ["copy:html"]
+        }
+      },
+
+      uglify: {
+        options: {
+          mangle: false
+        },
+        jsmini: {
+          files: [{
+            expand: true,
+            src: "js/**/*.js",
+            dest: "build"
+          }]
+        }
+      },
+
+      replace: {
+        inline: {
+          src: ["build/*.html"],
+          dest: "build/",
+          replacements: [{
+            from: "css/style.css",
+            to: "css/style.min.css"
+          }]
         }
       }
-    },
 
-    imagemin: {
-      images: {
-        options: {
-          optimizationLevel: 3
-        },
-      files: [{
-        expand:true,
-        src: ["build/img/**/*.{png,jpg,gif}"]
-      }]
-    }
-  });
+    });
 
-  grunt.registerTask("serve", ["browserSync", "watch"]);
-  grunt.registerTask("svgsprite", ["svgmin","svgstore"]);
-  grunt.registerTask("copy", ["copy"]);
-  grunt.registerTask("build", [
-    "clean",
-    "copy",
-    "less",
-    "postcss",
-    "cssmin",
-    "svgsprite",
-    "imagemin"
-  ])
-};
+    grunt.registerTask("serve", ["browserSync", "watch"]);
+    grunt.registerTask("deploy", ["less", "postcss"]);
+    grunt.registerTask("build", [
+      "clean",
+      "copy",
+      "less",
+      "postcss",
+      "csso",
+      "uglify",
+      "imagemin",
+      "replace"
+    ]);
+}
